9e0e576d8a029d82a6f64f7d18ab6fe76ba6cd3a
fedezz <federicomazzini.97@gmail.com>
2019-11-28 18:48:33 +0100

    prove mam

---
+++ 6.sendMAM.js
@@ -0,0 +1,38 @@
+//invio di messaggi ad un channel pubblico tangle
+
+const Mam = require('@iota/mam')
+const { asciiToTrytes, trytesToAscii } = require('@iota/converter')
+
+const mode = 'public'
+const provider = 'https://nodes.devnet.iota.org'
+
+
+// Initialise MAM State
+let mamState = Mam.init(provider)
+
+// Publish to tangle
+const publish = async packet => {
+
+    const trytes = asciiToTrytes(JSON.stringify(packet))
+    const message = Mam.create(mamState, trytes)
+
+    // Save new mamState
+    mamState = message.state
+
+    // Attach the payload
+    await Mam.attach(message.payload, message.address, 3, 9)
+
+    console.log('Published', packet, '\n');
+    console.log(message.root);
+    return message.root
+}
+
+cont = 0;
+setInterval(function(){
+cont++;
+const root = publish({
+    message: 'Message from Alice',
+    timestamp: (new Date()).toLocaleString(),
+    iter: cont
+  })
+}, 5000);
---
+++ 7.fetchMAM.js
@@ -0,0 +1,26 @@
+///////////////////////////////
+// MAM: Fetch messages to Public Stream
+///////////////////////////////
+
+const Mam = require('@iota/mam')
+const { trytesToAscii } = require('@iota/converter')
+
+const seed ='HYEWPPA9WIOAUS9GWNFOSUTIWBSZPARXXBTAFUEKIJZJBHMDGTFXMTXBUDOXRBBECAZTNENFHPF9TTCOQ';
+// Initialize MAM State - PUBLIC
+const mam = Mam.init('https://nodes.devnet.thetangle.org:443', seed)
+
+const root =
+  'LMRYISMSSC9KJUNUDJGLJZTDDIWNLI9BQJJWVOVXEZJAJSVMWGCCQK9NFRUDFOBAYMEVBLSSQMUKMEHRB'
+
+// Display coordinate data on our screen when we receive it
+const showData = raw => {
+  const data = trytesToAscii(raw)
+  console.log(data);
+}
+
+
+async function readMam() {
+  await Mam.fetch(root, 'public', null, showData)
+}
+
+readMam();
